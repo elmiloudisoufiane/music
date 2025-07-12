64b145c2ab34d9b2584311a238d4239e46792860
Soufiane Elmiloudi <elmiloudisofyan@gmail.com>
2025-07-12 06:50:21 +0100

    add Download page

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+      setTimeout(function () {
+        document.getElementById("loading").style.display = "none";
+        document.querySelector(".loader").style.display = "block";
+      }, 3000);
+    };
